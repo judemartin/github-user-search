79e470c5114a59f0a36b2250d4152184c30be911
Jude Martin <judemartin.mj@gmail.com>
2022-09-10 11:39:28 -0500

    adjusted margin hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,11 +3,11 @@ import ModeToggle from "./ModeToggle";
 export default function Hero() {
   return (
     <div className="pt-[31px] md:pt-[140px] 2xl:pt-[144px]">
-      <div className="mx-auto flex h-[38px] w-[327px] items-center text-white md:h-[38px] md:w-[573px] 2xl:h-[38px] 2xl:w-[730px]">
+      <div className="mx-auto flex h-[38px] w-[327px] items-center  text-white md:h-[38px] md:w-[573px] 2xl:h-[38px] 2xl:w-[730px]">
         <h3 className="text-left font-SpaceMono text-[26px] font-bold text-black dark:text-white">
           devfinder
         </h3>
-        <div className="mx-[100px] md:mx-[359px] 2xl:mx-[515px]">
+        <div className="mx-[92px] md:mx-[359px] 2xl:mx-[515px]">
           <ModeToggle />
         </div>
       </div>
